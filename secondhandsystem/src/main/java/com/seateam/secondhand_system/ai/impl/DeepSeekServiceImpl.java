9515983b616676874fce9ad3916cafffb9d41b43
RiverXue <x.x.x.x.x.x.x.x.x.h.k1@gmail.com>
2025-06-19 19:56:29 +0800

    ai模块基本完成

--- secondhandsystem/src/main/java/com/seateam/secondhand_system/ai/impl/DeepSeekServiceImpl.java
+++ secondhandsystem/src/main/java/com/seateam/secondhand_system/ai/impl/DeepSeekServiceImpl.java
@@ -3,7 +3,7 @@ package com.seateam.secondhand_system.ai.impl;
 import com.seateam.secondhand_system.ai.DeepSeekService;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.messages.UserMessage;
-import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -11,7 +11,7 @@ import reactor.core.publisher.Flux;
 public class DeepSeekServiceImpl implements DeepSeekService {
 
     @Resource
-    private OpenAiChatModel chatModel;
+    private DeepSeekChatModel chatModel;
 
     @Override
     public String chat(String message) {
